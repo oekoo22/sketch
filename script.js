2e372df91c5a3d7acd0a8777c22eb0f0ab006807
oekoo <y.dykow22@gmail.com>
2022-08-10 16:14:53 +0200

    Fix the Gap problem - classList isn t working -> add the styletext direct

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector('.container');
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
+  //div.classList.add("square");
   div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
   container.appendChild(div);
 }
